aea462911176a0d5874564481bb7e243a70ac115
Sujal Mahajan <145112750+RealSujal@users.noreply.github.com>
2025-07-18 17:24:57 +0530

    Update authMiddleware.js

--- community-backend/middleware/authMiddleware.js
+++ community-backend/middleware/authMiddleware.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const JWT_SECRET = process.env.JWT_SECRET || 'sQT8PcbHYUvPrci';
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const authMiddleware = (req, res, next) => {
     const authHeader = req.headers.authorization;
